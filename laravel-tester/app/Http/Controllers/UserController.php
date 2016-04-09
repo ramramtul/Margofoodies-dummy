d96883defeb2a55e0a826a6dc82715f509a6337c
rama rahmatullah <ramarahmatullah@gmail.com>
2016-04-09 15:10:01 +0700

    validasi1 4/9/2016

--- laravel-tester/app/Http/Controllers/UserController.php
+++ laravel-tester/app/Http/Controllers/UserController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-//use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use DB;
 use App\Http\Requests;
-use Request;
 use Validator;
 use App\Users;
 use Illuminate\Routing\Redirector;
-use //use Illuminate\Support\Facades\Request;
+ //use Illuminate\Support\Facades\Request;
 
 class UserController extends Controller
 {
@@ -17,12 +16,35 @@ class UserController extends Controller
     	return view('register');
     }
 
-    public function create(){
-        
-   	$input = $request -> all();
-    Users::create($input);
-    return redirect('home');
+    public function create(Request $request){
+       
+        Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+            'username' => 'required|min:3',
+        ]);
 
+        User::create([
+            'nama_lengkap' => $data['name'],
+            'email' => $data['email'],
+            'username' => $data['username'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        return redirect('home');
     }
+
+    public function val(array $request){
+        echo "string";
+        
+        // $input = $request->all();
+    }
+
+    public function store(array $data){
+        
+        
+    }
+
 }
      
